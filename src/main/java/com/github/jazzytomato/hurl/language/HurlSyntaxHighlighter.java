2957da989a1851f3cf8597d7bfb14b0624e2a698
Tom Haratyk <t.h@sent.com>
2024-05-30 09:52:47 +0100

    Syntax highlight working based on intellij "simple" language

---
+++ src/main/java/com/github/jazzytomato/hurl/language/HurlSyntaxHighlighter.java
@@ -0,0 +1,62 @@
+package com.github.jazzytomato.hurl.language;
+
+import com.github.jazzytomato.hurl.language.psi.HurlTypes;
+import com.intellij.lexer.Lexer;
+import com.intellij.openapi.editor.DefaultLanguageHighlighterColors;
+import com.intellij.openapi.editor.HighlighterColors;
+import com.intellij.openapi.editor.colors.TextAttributesKey;
+import com.intellij.openapi.fileTypes.SyntaxHighlighterBase;
+import com.intellij.psi.TokenType;
+import com.intellij.psi.tree.IElementType;
+import org.jetbrains.annotations.NotNull;
+
+import static com.intellij.openapi.editor.colors.TextAttributesKey.createTextAttributesKey;
+
+public class HurlSyntaxHighlighter extends SyntaxHighlighterBase {
+
+    public static final TextAttributesKey SEPARATOR =
+            createTextAttributesKey("HURL_SEPARATOR", DefaultLanguageHighlighterColors.OPERATION_SIGN);
+    public static final TextAttributesKey KEY =
+            createTextAttributesKey("HURL_KEY", DefaultLanguageHighlighterColors.KEYWORD);
+    public static final TextAttributesKey VALUE =
+            createTextAttributesKey("HURL_VALUE", DefaultLanguageHighlighterColors.STRING);
+    public static final TextAttributesKey COMMENT =
+            createTextAttributesKey("HURL_COMMENT", DefaultLanguageHighlighterColors.LINE_COMMENT);
+    public static final TextAttributesKey BAD_CHARACTER =
+            createTextAttributesKey("HURL_BAD_CHARACTER", HighlighterColors.BAD_CHARACTER);
+
+
+    private static final TextAttributesKey[] BAD_CHAR_KEYS = new TextAttributesKey[]{BAD_CHARACTER};
+    private static final TextAttributesKey[] SEPARATOR_KEYS = new TextAttributesKey[]{SEPARATOR};
+    private static final TextAttributesKey[] KEY_KEYS = new TextAttributesKey[]{KEY};
+    private static final TextAttributesKey[] VALUE_KEYS = new TextAttributesKey[]{VALUE};
+    private static final TextAttributesKey[] COMMENT_KEYS = new TextAttributesKey[]{COMMENT};
+    private static final TextAttributesKey[] EMPTY_KEYS = new TextAttributesKey[0];
+
+    @NotNull
+    @Override
+    public Lexer getHighlightingLexer() {
+        return new HurlLexerAdapter();
+    }
+
+    @Override
+    public TextAttributesKey @NotNull [] getTokenHighlights(IElementType tokenType) {
+        if (tokenType.equals(HurlTypes.SEPARATOR)) {
+            return SEPARATOR_KEYS;
+        }
+        if (tokenType.equals(HurlTypes.KEY)) {
+            return KEY_KEYS;
+        }
+        if (tokenType.equals(HurlTypes.VALUE)) {
+            return VALUE_KEYS;
+        }
+        if (tokenType.equals(HurlTypes.COMMENT)) {
+            return COMMENT_KEYS;
+        }
+        if (tokenType.equals(TokenType.BAD_CHARACTER)) {
+            return BAD_CHAR_KEYS;
+        }
+        return EMPTY_KEYS;
+    }
+
+}
---
+++ src/main/java/com/github/jazzytomato/hurl/language/HurlSyntaxHighlighterFactory.java
@@ -0,0 +1,17 @@
+package com.github.jazzytomato.hurl.language;
+
+import com.intellij.openapi.fileTypes.SyntaxHighlighter;
+import com.intellij.openapi.fileTypes.SyntaxHighlighterFactory;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import org.jetbrains.annotations.NotNull;
+
+final class HurlSyntaxHighlighterFactory extends SyntaxHighlighterFactory {
+
+    @NotNull
+    @Override
+    public SyntaxHighlighter getSyntaxHighlighter(Project project, VirtualFile virtualFile) {
+        return new HurlSyntaxHighlighter();
+    }
+
+}
