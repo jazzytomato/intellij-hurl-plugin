df743411a1e4461128edffd96d0503d24cc6ecaa
Tom Haratyk <t.h@sent.com>
2024-06-24 20:58:54 +0100

    Update icon

--- src/main/java/com/github/jazzytomato/hurl/language/HurlIcons.java
+++ src/main/java/com/github/jazzytomato/hurl/language/HurlIcons.java
@@ -1,10 +1,11 @@
 package com.github.jazzytomato.hurl.language;
 
 import com.intellij.icons.AllIcons;
+import com.intellij.openapi.util.IconLoader;
 
 import javax.swing.*;
 
 public class HurlIcons {
 
-    public static final Icon FILE = AllIcons.Diff.ArrowLeftRight;
+    public static final Icon FILE = IconLoader.getIcon("/icons/hurlIcon.svg", HurlIcons.class);
 }
